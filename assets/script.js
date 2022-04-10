3dac4d9c60da59a25d8fbcd8d55861dd5ac09953
GianniMejia <Giannimejia12@yahoo.com>
2022-04-10 13:52:57 -0700

    added loop to ensure variable was chosen

--- assets/script.js
+++ assets/script.js
@@ -8,15 +8,26 @@ var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K
 var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+
+//Confirm how many characters will be included in the password
+function generatePassword() {
+  while(confirmLength <= 8 || confirmLength >= 128)
+  var confirmLength = (prompt("How many characters do you want your password to include?"));
+  }
+  
+  //Loop if the chosen amount of characters is less or more than the reqired amount
+  while(confirmLength <= 8 || confirmLength >= 128) {
+    alert("Password length must be between 8-128 characters. Please choose agaon.");
+    var confirmLength = (prompt("How many characters do you want your password to include?"));
+  }
+
+  
+
 //Variables to stire selected character types
 var hasLowerCased = confirm("Click ok to include lower cased characters");
 var hasUpperCased = confirm("Click ok to include upper cased characters");
 var hasSpecial = confirm("Click ok to include special characters");
 var hasNumbers = confirm("Click ok to include numbers");
-
-
-
-
 
 
 
