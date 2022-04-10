2ad4752f9584154ab42733340019f2ef1c04b2de
GianniMejia <Giannimejia12@yahoo.com>
2022-04-10 13:18:22 -0700

    added confirm password and character limits

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 // Assignment code here
 
 
+
+//Arrays for letters, numbers, and special scharacters
+var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharacters = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+//Variables to stire selected character types
+var hasLowerCased = confirm("Click ok to include lower cased characters");
+var hasUpperCased = confirm("Click ok to include upper cased characters");
+var hasSpecial = confirm("Click ok to include special characters");
+var hasNumbers = confirm("Click ok to include numbers");
+
+
+
+
+
+
+
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
